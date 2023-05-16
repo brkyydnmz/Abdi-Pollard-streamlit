9e61cfb20208535c4dc39586ff8f4f70229ccd36
Berkay Dönmez <berkaydonmez1601@gmail.com>
2023-05-16 10:43:27 +0300

    first commit

--- app.py
+++ app.py
@@ -95,20 +95,22 @@ def main():
        [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
        [Kaggle](https://www.kaggle.com/avikumart) 
        """)
-       a,b,c = st.columns([0.2,0.6,0.2])
+a, b, c = st.columns([0.2, 0.6, 0.2])
+
 with b:
- st.image("banner-picture.jpeg", use_column_width=True)
+    st.image("banner-picture.jpeg", use_column_width=True)
 
 
-# description about the project and code files       
-st.subheader("🧾Description:")
-st.text("""This data set is collected from Addis Ababa Sub-city police departments for master's research work. 
+    # description about the project and code files       
+    st.subheader("🧾Description:")
+    st.text("""This data set is collected from Addis Ababa Sub-city police departments for master's research work. 
 The data set has been prepared from manual records of road traffic accidents of the year 2017-20. 
 All the sensitive information has been excluded during data encoding and finally it has 32 features and 12316 instances of the accident.
 Then it is preprocessed and for identification of major causes of the accident by analyzing it using different machine learning classification algorithms.
-""")
+    """
+    )
 
-st.markdown("Source of the dataset: [Click Here](https://www.narcis.nl/dataset/RecordID/oai%3Aeasy.dans.knaw.nl%3Aeasy-dataset%3A191591)")
+    st.markdown("Source of the dataset: [Click Here](https://www.narcis.nl/dataset/RecordID/oai%3Aeasy.dans.knaw.nl%3Aeasy-dataset%3A191591)")
 
 st.subheader("🧭 Problem Statement:")
 st.text("""The target feature is Accident_severity which is a multi-class variable. 
